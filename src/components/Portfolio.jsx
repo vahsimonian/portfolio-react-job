9b6f0626b509233cf440e2a2ae30c1029e71bf3c
vahsimonian <simonianvah@gmail.com>
2023-08-20 14:54:11 +0700

    commit

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -30,14 +30,6 @@ const Portfolio = () => {
                 />
               </Link>
               <p className='font-bold ml-2'>{desc}</p>
-              {/* <div className='flex items-center justify-center'> */}
-              {/* <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>
-                  Demo
-                </button>
-                <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>
-                  Code
-                </button> */}
-              {/* </div> */}
             </div>
           ))}
         </div>
