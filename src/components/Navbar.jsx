a4b249264357bf1051f183f1b9e3caf48d509170
vahsimonian <simonianvah@gmail.com>
2023-09-29 13:38:47 +0700

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,7 +25,7 @@ const NavBar = () => {
   ]
 
   return (
-    <div className='flex justify-between items-center w-full h-20 px-4 text-white bg-black '>
+    <div className='flex justify-between items-center w-full h-20 px-4 text-white bg-black'>
       <div>
         <h1 className='text-4xl font-signature ml-2'>Vah Simon</h1>
       </div>
