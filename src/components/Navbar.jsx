5ef30e39aebc12312ccf84ee7506ac62ac9105c9
vahsimonian <simonianvah@gmail.com>
2023-09-29 13:51:21 +0700

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,8 +25,8 @@ const NavBar = () => {
   ]
 
   return (
-    <div className='flex justify-between items-center w-full h-20 px-4 text-white bg-black'>
-      <div   >
+    <div className='flex justify-between items-center w-full h-20 px-4 text-white bg-black navbar-mobile'>
+      <div>
         <h1 className='text-4xl font-signature ml-2'>Vah Simon</h1>
       </div>
 
@@ -57,7 +57,7 @@ const NavBar = () => {
         onClick={() => setNav(!nav)}
         className='cursor-pointer pr-4 z-10 text-gray-500 md:hidden'
       >
-        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+        {/* {nav ? <FaTimes size={30} /> : <FaBars size={30} />} */}
       </div>
       {nav && (
         <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500'>
