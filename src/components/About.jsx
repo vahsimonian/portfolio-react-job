2988abc157625cde6ca17a91e75e6681df58f905
vahsimonian <simonianvah@gmail.com>
2023-10-10 19:40:41 +0700

    commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,32 +1,33 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
     <div
-      name='about'
-      className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
     >
-      <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
-        <div className='pb-8'>
-          <p className='text-4xl font-bold inline border-b-4 border-gray-500'>
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full about-page">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             About
           </p>
         </div>
 
-        <p className='text-xl mt-20'>
-          Greetings! I'm Vah, an Armenian expatriate  and currently residing in the vibrant landscapes of Cambodia,
-          nestled in Southeast Asia. My journey has taken me across borders and
-          cultures, spanning from China to Russia, and eventually leading me to
-          the enchanting town of Kampot. Since 2018, I've been passionately
-          imparting knowledge in the realms of English and Math, channeling my
-          expertise across diverse international settings. As I guided eager
-          minds through the intricacies of these subjects, I found myself drawn
-          to the captivating world of web development.
+        <p className="text-xl mt-20">
+          Greetings! I'm Vah, an Armenian expatriate and currently residing in
+          the vibrant landscapes of Cambodia, nestled in Southeast Asia. My
+          journey has taken me across borders and cultures, spanning from China
+          to Russia, and eventually leading me to the enchanting town of Kampot.
+          Since 2018, I've been passionately imparting knowledge in the realms
+          of English and Math, channeling my expertise across diverse
+          international settings. As I guided eager minds through the
+          intricacies of these subjects, I found myself drawn to the captivating
+          world of web development.
         </p>
 
         <br />
 
-        <p className='text-xl'>
+        <p className="text-xl">
           In late 2021, I embarked on a new odyssey, delving into the intricate
           realms of HTML, CSS, and JavaScript. With each passing day, I've
           diligently nurtured these skills, witnessing their evolution from
@@ -41,7 +42,7 @@ const About = () => {
         </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
