6c5596c23d96ff89e9e8e0692000ed8feeea3aed
vahsimonian <simonianvah@gmail.com>
2023-10-10 16:52:22 +0700

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,51 +1,51 @@
-import React, { useState } from 'react'
-import { FaBars, FaTimes } from 'react-icons/fa'
-import { NavLink } from 'react-router-dom'
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
-  const [nav, setNav] = useState(false)
+  const [nav, setNav] = useState(false);
 
   const links = [
     {
       id: 1,
-      link: 'about',
+      link: "about",
     },
     {
       id: 2,
-      link: 'portfolio',
+      link: "portfolio",
     },
     {
       id: 3,
-      link: 'experience',
+      link: "experience",
     },
     {
       id: 4,
-      link: 'contact',
+      link: "contact",
     },
-  ]
+  ];
 
   return (
-    <div className='flex justify-between items-center w-full h-20 px-4 text-white bg-black navbar-mobile'>
+    <div className="items-center w-full h-20 px-4 text-white bg-black navbar-mobile">
       <div>
-        <h1 className='text-4xl font-signature ml-2'>Vah Simon</h1>
+        <h1 className="text-4xl font-signature ml-2">Vah Simon</h1>
       </div>
 
-      <ul className='flex navbar'>
+      <ul className="flex navbar">
         <NavLink
-          className={({ isActive }) => (isActive ? 'active-link' : null)}
-          to={'/'}
+          className={({ isActive }) => (isActive ? "active-link" : null)}
+          to={"/"}
         >
-          <li className='px-4 cursor-pointer capitalize font-medium text-gray-500'>
+          <li className="px-4 cursor-pointer capitalize font-medium text-gray-500">
             Home
           </li>
         </NavLink>
         {links.map(({ id, link }) => (
           <li
             key={id}
-            className='px-4 cursor-pointer capitalize font-medium text-gray-500'
+            className="px-4 cursor-pointer capitalize font-medium text-gray-500"
           >
             <NavLink
-              className={({ isActive }) => (isActive ? 'active-link' : null)}
+              className={({ isActive }) => (isActive ? "active-link" : null)}
               to={link}
             >
               {link}
@@ -55,20 +55,20 @@ const NavBar = () => {
       </ul>
       <div
         onClick={() => setNav(!nav)}
-        className='cursor-pointer pr-4 z-10 text-gray-500 md:hidden'
+        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
       >
         {/* {nav ? <FaTimes size={30} /> : <FaBars size={30} />} */}
       </div>
       {nav && (
-        <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500'>
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
           {links.map(({ id, link }) => (
             <li
               key={id}
-              className='px-4 cursor-pointer capitalize py-6 text-4xl'
+              className="px-4 cursor-pointer capitalize py-6 text-4xl"
             >
               <NavLink
                 to={link}
-                className={({ isActive }) => (isActive ? 'active-link' : null)}
+                className={({ isActive }) => (isActive ? "active-link" : null)}
               >
                 {link}
               </NavLink>
@@ -77,7 +77,7 @@ const NavBar = () => {
         </ul>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
