2123c8c4665a7bb02f7f777c4a37b9eb433fa8d1
vahsimonian <simonianvah@gmail.com>
2023-10-10 17:49:12 +0700

    commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,10 +8,10 @@ const Home = () => {
   return (
     <div
       name="home"
-      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 home-screen"
+      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
       id="home-mobile"
     >
-      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row home-screen">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
             I'm a Front-End Developer
@@ -41,7 +41,7 @@ const Home = () => {
           <img
             src={HeroImage}
             alt="my profile"
-            className="rounded-2xl mx-auto w-2/3 md:w-full"
+            className="rounded-2xl mx-auto w-2/3 md:w-full home-img"
           />
         </div>
       </div>
