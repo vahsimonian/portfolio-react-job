918c755f40979673e19a530ee3b32979287d6d19
vahsimonian <simonianvah@gmail.com>
2023-10-11 17:03:56 +0700

    commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,34 +12,36 @@ const About = () => {
             About
           </p>
         </div>
+        <div className="about-text">
+          <p className="text-xl mt-20">
+            Greetings! I'm Vah, an Armenian expatriate and currently residing in
+            the vibrant landscapes of Cambodia, nestled in Southeast Asia. My
+            journey has taken me across borders and cultures, spanning from
+            China to Russia, and eventually leading me to the enchanting town of
+            Kampot. Since 2018, I've been passionately imparting knowledge in
+            the realms of English and Math, channeling my expertise across
+            diverse international settings. As I guided eager minds through the
+            intricacies of these subjects, I found myself drawn to the
+            captivating world of web development.
+          </p>
 
-        <p className="text-xl mt-20">
-          Greetings! I'm Vah, an Armenian expatriate and currently residing in
-          the vibrant landscapes of Cambodia, nestled in Southeast Asia. My
-          journey has taken me across borders and cultures, spanning from China
-          to Russia, and eventually leading me to the enchanting town of Kampot.
-          Since 2018, I've been passionately imparting knowledge in the realms
-          of English and Math, channeling my expertise across diverse
-          international settings. As I guided eager minds through the
-          intricacies of these subjects, I found myself drawn to the captivating
-          world of web development.
-        </p>
+          <br />
 
-        <br />
-
-        <p className="text-xl">
-          In late 2021, I embarked on a new odyssey, delving into the intricate
-          realms of HTML, CSS, and JavaScript. With each passing day, I've
-          diligently nurtured these skills, witnessing their evolution from
-          nascent concepts to formidable tools in my arsenal. This continuous
-          refinement has now reached a point where I stand poised to embrace the
-          role of a junior web developer. The intersection of creativity and
-          logic in web development resonates deeply with me, and I'm thrilled to
-          embark on this exciting chapter. If you're seeking a dedicated and
-          eager learner with an unwavering commitment to growth, I would be
-          truly honored to engage in a conversation with you. Thank you for your
-          invaluable time and consideration. Warm regards, Vah
-        </p>
+          <p className="text-xl">
+            In late 2021, I embarked on a new odyssey, delving into the
+            intricate realms of HTML, CSS, and JavaScript. With each passing
+            day, I've diligently nurtured these skills, witnessing their
+            evolution from nascent concepts to formidable tools in my arsenal.
+            This continuous refinement has now reached a point where I stand
+            poised to embrace the role of a junior web developer. The
+            intersection of creativity and logic in web development resonates
+            deeply with me, and I'm thrilled to embark on this exciting chapter.
+            If you're seeking a dedicated and eager learner with an unwavering
+            commitment to growth, I would be truly honored to engage in a
+            conversation with you. Thank you for your invaluable time and
+            consideration. Warm regards, Vah
+          </p>
+        </div>
       </div>
     </div>
   );
