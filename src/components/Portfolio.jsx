ba04773faf420f51efcdf088d5e699be864804bf
vahsimonian <simonianvah@gmail.com>
2023-10-11 08:30:55 +0700

    commit

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,36 +1,36 @@
-import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
-import { portfolios } from '../data'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { portfolios } from "../data";
 
 const Portfolio = () => {
   return (
     <div
-      name='portfolio'
-      className='bg-gradient-to-b from-black to-gray-800 w-full text-white sm:h-screen'
+      name="portfolio"
+      className="bg-gradient-to-b from-black to-gray-800 w-full text-white sm:h-screen"
     >
-      <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
-        <NavLink to={'/'} className='mb-2 underline hover:no-underline'>
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full portfolio-page">
+        <NavLink to={"/"} className="mb-2 underline hover:no-underline">
           <p>back home</p>
         </NavLink>
-        <div className='pb-8'>
-          <p className='text-4xl font-bold inline border-b-4 border-gray-500'>
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Portfolio
           </p>
-          <p className='py-6'>Check out some of my work right here</p>
+          <p className="py-6">Check out some of my work right here</p>
         </div>
 
-        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
           {portfolios.map(({ id, src, linkTo, desc, codeLink }) => (
-            <div key={id} className='shadow-md mt-8 shadow-gray-600 rounded-lg'>
-              <Link to={`${linkTo}`} target='_blank'>
+            <div key={id} className="shadow-md mt-8 shadow-gray-600 rounded-lg">
+              <Link to={`${linkTo}`} target="_blank">
                 <img
                   src={src}
-                  alt=''
-                  className='rounded-md duration-200 hover:scale-105'
+                  alt=""
+                  className="rounded-md duration-200 hover:scale-105"
                 />
               </Link>
-              <Link to={codeLink} target='_blank' className='mt-4 absolute'>
-                <p style={{ fontSize: '12.5px' }} className='font-bold ml-2'>
+              <Link to={codeLink} target="_blank" className="mt-4 absolute">
+                <p style={{ fontSize: "12.5px" }} className="font-bold ml-2">
                   code source...
                 </p>
               </Link>
@@ -39,7 +39,7 @@ const Portfolio = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
