b9ca9c9828526b80f463dfc15db488d9204d7d1e
vahsimonian <simonianvah@gmail.com>
2023-10-11 17:11:23 +0700

    commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <div
       name="contact"
-      className="w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white contact-div"
+      className="w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white contact-page"
     >
       <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full contact-page">
         <div className="pb-8">
